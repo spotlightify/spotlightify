7859d23c35e32e102b68c1ae02eec6cad50bb85e
Rob <rob@wilkie.io>
2020-06-02 19:20:07 +0100

    Renamed some colours

--- colors.py
+++ colors.py
@@ -1,9 +1,9 @@
 class colors:
-    HEADER = '\033[95m'
+    PINK = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     RESET = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
