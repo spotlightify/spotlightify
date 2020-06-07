820eb7f5193dc61e291ad412abfe413efc6dbd4d
Peter Murphy <petergmurphy212@gmail.com>
2020-06-08 00:35:46 +0100

    Update holder.py
    checks if holder needs to be refreshed

--- spotlight/commands/holder.py
+++ spotlight/commands/holder.py
@@ -1,5 +1,6 @@
 import json
 from os import sep
+from datetime import datetime, timedelta
 from definitions import CACHE_DIR
 
 
@@ -8,30 +9,43 @@ class CacheHolder:
     song_cache = {}
     artist_cache = {}
     album_cache = {}
+    last_refresh = datetime.now()
 
     @staticmethod
-    def refresh_holder(_type: str):
+    def check_reload_time(_type: str):  # will refresh every 5 minutes (300 seconds)
+        time_passed = (datetime.now() - CacheHolder.last_refresh).total_seconds()
+        print("checking reload")
+        if time_passed > 300:
+            print("reload is a go")
+            CacheHolder.last_refresh = datetime.now()
+            CacheHolder.reload_holder(_type)
+
+    @staticmethod
+    def reload_holder(_type: str):
         '''
         Reloads data to static variables inside of this class
         :param _type: Either song, playlist, artist, album or all
         :return:
         '''
         # load cached songs
-        if _type == "song" or _type == "all":
-            with open(song_cache_file_path, 'r') as f:
-                CacheHolder.song_cache = json.load(f)
-        # load cached artists
-        if _type == "artist" or _type == "all":
-            with open(artist_cache_file_path, 'r') as f:
-                CacheHolder.artist_cache = json.load(f)
-        # load cached albums
-        if _type == "album" or _type == "all":
-            with open(album_cache_file_path, 'r') as f:
-                CacheHolder.album_cache = json.load(f)
-        # load cached playlists
-        if _type == "playlist" or _type == "all":
-            with open(playlist_cache_file_path, 'r') as f:
-                CacheHolder.playlist_cache = json.load(f)
+        try:
+            if _type == "song" or _type == "all":
+                with open(song_cache_file_path, 'r') as f:
+                    CacheHolder.song_cache = json.load(f)
+            # load cached artists
+            if _type == "artist" or _type == "all":
+                with open(artist_cache_file_path, 'r') as f:
+                    CacheHolder.artist_cache = json.load(f)
+            # load cached albums
+            if _type == "album" or _type == "all":
+                with open(album_cache_file_path, 'r') as f:
+                    CacheHolder.album_cache = json.load(f)
+            # load cached playlists
+            if _type == "playlist" or _type == "all":
+                with open(playlist_cache_file_path, 'r') as f:
+                    CacheHolder.playlist_cache = json.load(f)
+        except:
+            None  # First time startup with no cache
 
 
 # Cached file paths
