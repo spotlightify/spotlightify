91d79429e797fc523aa8e279d54ee832277ed8f0
Peter Murphy <petergmurphy212@gmail.com>
2020-07-12 02:31:42 +0100

    Added auth functionality and styling

--- auth/ui.py
+++ auth/ui.py
@@ -1,13 +1,17 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QDialogButtonBox, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QDialogButtonBox, QWidget, QLabel, QLineEdit, QErrorMessage, \
+    QMessageBox
 from PyQt5.QtCore import Qt, QRect
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QIcon
 
+from settings.themes import default_themes
+from definitions import ASSETS_DIR
+from os import sep
 from auth import Config
 
 
 class AuthUI(QDialog):
     def __init__(self, config: Config):
-        QDialog.__init__(self)
+        QDialog.__init__(self, None, Qt.WindowTitleHint)
 
         self.config = config
 
@@ -19,31 +23,38 @@ class AuthUI(QDialog):
         }
 
         self.setWindowModality(Qt.ApplicationModal)
-        self.setFixedSize(340, 233)
+        self.setFixedSize(450, 233)
+        self.setWindowTitle("Spotlightify - Authentication")
+        self.setWindowIcon(QIcon(f"{ASSETS_DIR}svg{sep}cog"))
+
+        self.setStyleSheet(f"background-color: {default_themes['dark'].background}; color: {default_themes['dark'].foreground}")
 
         self.layout_widget = VerticalLayout(self)
-        self.layout_widget.setGeometry(QRect(10, 10, 300, 160))
+        self.layout_widget.setGeometry(QRect(10, 10, 440, 160))
 
-        self.layout_widget.add(InputField(label="Username", store=self.cfg, field="username", set_=self.field_changed))
+        self.layout_widget.add(InputField(label="Username", store=self.cfg, field="username"))
         self.layout_widget.add(
-            InputField(label="Client ID", store=self.cfg, field="client_id", set_=self.field_changed))
+            InputField(label="Client ID", store=self.cfg, field="client_id", required_length=32))
         self.layout_widget.add(
-            InputField(label="Client Secret", store=self.cfg, field="client_secret", set_=self.field_changed))
+            InputField(label="Client Secret", store=self.cfg, field="client_secret", required_length=32))
         self.layout_widget.add(
-            InputField(label="Redirect URI", store=self.cfg, field="redirect_uri", set_=self.field_changed))
+            InputField(label="Redirect URI", store=self.cfg, field="redirect_uri", max_length=100))
 
         self.buttonBox = QDialogButtonBox(self)
         self.buttonBox.setGeometry(QRect(10, 190, 300, 23))
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Save)
         self.buttonBox.accepted.connect(self.save_changes)
         self.buttonBox.rejected.connect(self.hide)
 
-    def field_changed(self, field, value):
-        self.cfg[field] = value
-
     def save_changes(self):
-        self.config.set_all(self.cfg["username"], self.cfg["client_id"], self.cfg["client_secret"],
-                            self.cfg["redirect_uri"])
+        widgets = self.layout_widget.children()
+        if all([w.text_complete for w in widgets]):
+            self.config.set_all(self.cfg["username"], self.cfg["client_id"], self.cfg["client_secret"],
+                                self.cfg["redirect_uri"])
+            self.hide()
+        else:
+            QMessageBox.warning(self, "Error", "The fields below have not been filled in correctly:\n" +
+                                 '\n'.join([w.label.text() for w in widgets if not w.text_complete]), QMessageBox.Ok)
 
 
 class VerticalLayout(QWidget):
@@ -54,11 +65,20 @@ class VerticalLayout(QWidget):
     def add(self, widget: QWidget):
         self.vertical_layout.addWidget(widget)
 
+    def children(self):
+        return super(VerticalLayout, self).children()[1:]
+
 
 class InputField(QWidget):
-    def __init__(self, label: str, store: dict, field: str, set_: classmethod):
+    def __init__(self, label: str, store: dict, field: str, max_length=32, required_length=1):
         QWidget.__init__(self)
-        self.setFixedSize(300, 40)
+        self.setFixedSize(400, 300)
+
+        self.max_length = max_length
+        self.required_length = required_length
+        self.text_complete = False
+        self.store = store
+        self.field = field
 
         self.font = QFont()
         self.font.setFamily("SF Pro Display")
@@ -70,13 +90,20 @@ class InputField(QWidget):
         self.label.setText(label)
 
         self.textbox = QLineEdit(self)
-        self.textbox.setGeometry(QRect(110, 5, 180, 30))
+        self.textbox.setGeometry(QRect(110, 5, 290, 30))
         self.textbox.setFont(self.font)
+        self.textbox.textChanged.connect(self.field_changed)
         self.textbox.setText(store[field])
-        self.textbox.setFrame(False)
-        self.textbox.textChanged.connect(lambda val: set_(field, val))
+        self.textbox.setMaxLength(max_length)
 
-# app = QApplication([])
-# ui = AuthUI(Config())
-# ui.show()
-# app.exec_()
+    def field_changed(self):
+        text = self.textbox.text()
+
+        self.store[self.field] = text
+
+        if len(text) >= self.required_length:
+            self.text_complete = True
+            self.textbox.setStyleSheet("border: 1px solid #006600;")
+        else:
+            self.text_complete = False
+            self.textbox.setStyleSheet("border: 1px solid #4d0000;")
---
+++ tests/authui.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QApplication
+from auth.ui import AuthUI
+from auth.config import Config
+
+cfg = Config()
+app = QApplication([])
+window = AuthUI(cfg)
+window.show() # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+window.exec_()
