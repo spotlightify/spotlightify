d79d299d8eb7dd48f6ceba5cfa7a2bbf81f7cbe1
Peter Murphy <petergmurphy212@gmail.com>
2020-10-05 19:47:58 +0100

    Removed Item Suggestion
    Class was deprecated

--- spotlight/suggestions/item.py
+++
@@ -1,7 +0,0 @@
-from spotlight.suggestions.suggestion import Suggestion
-
-
-class Item(Suggestion):
-    def __init__(self, title: str, description: str, icon_name: str, function: classmethod, fill_str: str,
-                 parameter, setting: str):
-        Suggestion.__init__(self, title, description, icon_name, function, fill_str, parameter, setting)
