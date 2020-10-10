488bdaa5afd4543052742c5a286785fa4a6e33f3
Mark McMoran <mark.mcmoran@live.com>
2020-10-10 15:06:00 +0100

    Rm'ed Environment variable func...
    The open_env func is pointless, it was fine when this class was
    barebones i.e. no OOP design in place...

--- auth/config.py
+++ auth/config.py
@@ -24,17 +24,10 @@ class Config:
         self._scope = " ".join(scopes)
         self.config_path = f"{CACHE_DIR}config.json"
 
-        self.open_env()
         if not self.is_valid():
             self.open_json()
 
-    def open_env(self):
-        if all(p in environ for p in
-               ["SPOTIPY_CLIENT_ID", "SPOTIPY_CLIENT_SECRET", "SPOTIPY_REDIRECT_URI", "USERNAME"]):
-            self._username = environ["USERNAME"]
-            self._client_id = environ["SPOTIPY_CLIENT_ID"]
-            self._client_secret = environ["SPOTIPY_CLIENT_SECRET"]
-            self._redirect_uri = environ["SPOTIPY_REDIRECT_URI"]
+
 
     def open_json(self):
         try:
@@ -79,7 +72,7 @@ class Config:
         if len(self._redirect_uri) == 0:
             return False
 
-        # return True
+        return True
 
     @property
     def username(self):
