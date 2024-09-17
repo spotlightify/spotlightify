ccc89adf7ae64b810f12b2edc4b21a51d6f87160
petergmurphy <57573942+petergmurphy@users.noreply.github.com>
2024-09-17 20:59:20 +0100

    Fix suggestion list movement
    This commit fixes list movement when moving up and down through the list
    of suggestions.

--- frontend/src/components/Suggestion/SuggestionsContainer.tsx
+++ frontend/src/components/Suggestion/SuggestionsContainer.tsx
@@ -8,35 +8,73 @@ interface SuggestionContainerProps {
   actionHandler: (action: SuggestionAction) => void;
 }
 
+interface VisibleSuggestions {
+  startIndex: number;
+  endIndex: number;
+  focusedIndex: number;
+}
+
 function SuggestionsContainer({
   suggestions,
   actionHandler,
 }: SuggestionContainerProps) {
-  const [focusedSuggestionIndex, setFocusedSuggestionIndex] = useState(0);
   const suggestionsRef = useRef<HTMLDivElement>(null);
+  const [visibleSuggestions, setVisibleSuggestions] =
+    useState<VisibleSuggestions>({
+      startIndex: 0,
+      endIndex: 0,
+      focusedIndex: 0,
+    });
 
   useEffect(() => {
-    setFocusedSuggestionIndex(0);
+    setVisibleSuggestions({
+      startIndex: 0,
+      endIndex: Math.min(suggestions.length - 1, 7),
+      focusedIndex: 0,
+    });
   }, [suggestions]);
 
   useEffect(() => {
-    if (suggestionsRef.current && focusedSuggestionIndex > 7) {
-      suggestionsRef.current.scrollTop = 58 * (focusedSuggestionIndex - 7);
-    } else if (suggestionsRef.current) {
-      suggestionsRef.current.scrollTop = 0;
+    if (suggestionsRef.current) {
+      suggestionsRef.current.scrollTop = 58 * visibleSuggestions.startIndex;
     }
-  }, [focusedSuggestionIndex]);
+  }, [visibleSuggestions.focusedIndex]);
 
   useEffect(() => {
     const moveFocusedIndex = (direction: "up" | "down") => {
+      const noOfVisibleSuggestions = Math.min(suggestions.length, 8);
+      let newFocusedIndex = 0;
       if (direction === "up") {
-        setFocusedSuggestionIndex((prevIndex) =>
-          prevIndex === 0 ? suggestions.length - 1 : prevIndex - 1
-        );
+        newFocusedIndex =
+          visibleSuggestions.focusedIndex === 0
+            ? suggestions.length - 1
+            : visibleSuggestions.focusedIndex - 1;
       } else {
-        setFocusedSuggestionIndex((prevIndex) =>
-          prevIndex === suggestions.length - 1 ? 0 : prevIndex + 1
-        );
+        newFocusedIndex =
+          visibleSuggestions.focusedIndex === suggestions.length - 1
+            ? 0
+            : visibleSuggestions.focusedIndex + 1;
+      }
+
+      if (newFocusedIndex > visibleSuggestions.endIndex) {
+        const newEndIndex = newFocusedIndex;
+        setVisibleSuggestions({
+          endIndex: newEndIndex,
+          startIndex: newEndIndex - (noOfVisibleSuggestions - 1),
+          focusedIndex: newFocusedIndex,
+        });
+      } else if (newFocusedIndex < visibleSuggestions.startIndex) {
+        const newStartIndex = newFocusedIndex;
+        setVisibleSuggestions({
+          startIndex: newStartIndex,
+          endIndex: newStartIndex + (noOfVisibleSuggestions - 1),
+          focusedIndex: newFocusedIndex,
+        });
+      } else {
+        setVisibleSuggestions((prevSuggestions) => ({
+          ...prevSuggestions,
+          focusedIndex: newFocusedIndex,
+        }));
       }
     };
 
@@ -50,7 +88,7 @@ function SuggestionsContainer({
         moveFocusedIndex("up");
       }
       if (event.key === "Enter" && suggestions.length > 0) {
-        const action = suggestions[focusedSuggestionIndex].action;
+        const action = suggestions[visibleSuggestions.focusedIndex].action;
         if (action) {
           actionHandler(action);
         }
@@ -62,13 +100,13 @@ function SuggestionsContainer({
 
     // Remove the event listener on cleanup
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [actionHandler, focusedSuggestionIndex, suggestions]);
+  }, [actionHandler, visibleSuggestions.focusedIndex, suggestions]);
 
   const suggestionElements = suggestions.map((suggestion, index) => (
     <SuggestionElement
       key={suggestion.id}
       suggestion={suggestion}
-      isFocused={index === focusedSuggestionIndex}
+      isFocused={index === visibleSuggestions.focusedIndex}
       handleAction={() => {
         if (suggestion.action) {
           actionHandler(suggestion.action);
