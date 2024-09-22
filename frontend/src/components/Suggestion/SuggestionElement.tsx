f855b173150feb26a56275d19c05ad3d621d24db
petergmurphy <57573942+petergmurphy@users.noreply.github.com>
2024-09-22 17:24:37 +0100

    Implement suggestion options WIP
    This commit implements suggestion options. Suggestions will now be able
    to contain multiple additional descriptions and actions which will be
    accessible when a suggestion is focused and the appropriate modifier key
    {shift, ctrl, alt} is pressed.

--- frontend/src/components/Suggestion/SuggestionElement.tsx
+++ frontend/src/components/Suggestion/SuggestionElement.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { Suggestion } from "../../types/command";
+import { ModKeys, Suggestion, SuggestionAction } from "../../types/command";
 
 interface SuggestionProps {
   suggestion: Suggestion;
   isFocused: boolean;
-  handleAction: () => void;
+  handleAction: (action: SuggestionAction) => void;
+  modifierKey?: ModKeys;
 }
 
 function SuggestionElement({
   suggestion,
   isFocused = false,
   handleAction,
+  modifierKey,
 }: SuggestionProps) {
+  let action = suggestion.action;
+  let description = suggestion.description;
+  if (isFocused && modifierKey) {
+    action = suggestion.modKeys?.[modifierKey]?.action;
+    description = suggestion.modKeys?.[modifierKey]?.description ?? description;
+  }
+
   return (
     <button
       type="button"
       className={`suggestion-item ${isFocused ? "button--focus" : ""}`}
-      onClick={handleAction}
+      onClick={() => (action !== undefined ? handleAction(action) : undefined)}
     >
       <div className="suggestion-item__icon-wrapper">
         <img
@@ -29,9 +38,7 @@ function SuggestionElement({
       </div>
       <div className="suggestion-text-wrapper">
         <div className="suggestion-item__title">{suggestion.title}</div>
-        <div className="suggestion-item__description">
-          {suggestion.description}
-        </div>
+        <div className="suggestion-item__description">{description}</div>
       </div>
     </button>
   );
